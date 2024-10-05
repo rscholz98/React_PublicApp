6e489142a5853102a6285362924bac7b10e3d59a
rscholz <richardscholz1@gmx.de>
2024-10-05 11:50:46 +0200

    - Add

---
+++ app/src/internals/components/Copyright.js
@@ -0,0 +1,28 @@
+/** @format */
+
+import * as React from "react";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+
+export default function Copyright(props) {
+ return (
+  <Typography
+   variant="body2"
+   align="center"
+   {...props}
+   sx={[
+    {
+     color: "text.secondary",
+    },
+    ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
+   ]}
+  >
+   {"Copyright © "}
+   <Link color="inherit" href="https://mui.com/">
+    XXX
+   </Link>{" "}
+   {new Date().getFullYear()}
+   {"."}
+  </Typography>
+ );
+}
